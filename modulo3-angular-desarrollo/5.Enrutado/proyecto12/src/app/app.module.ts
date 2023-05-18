425b2ae20a5bd19c6db9bf9d5832d784f8838987
AlbanisGonzalez <albanisy@gmail.com>
2023-05-18 10:15:46 +0200

    creacion de modulo

--- modulo3-angular-desarrollo/5.Enrutado/proyecto12/src/app/app.module.ts
+++ modulo3-angular-desarrollo/5.Enrutado/proyecto12/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
+import { PhotoGalleryComponent } from './photos/photo-gallery/photo-gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoGalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
