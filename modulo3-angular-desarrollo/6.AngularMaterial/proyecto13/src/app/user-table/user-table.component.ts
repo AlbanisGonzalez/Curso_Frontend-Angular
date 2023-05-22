fd7f6d17ae2eabf4f43af874f8408f96a62db0da
AlbanisGonzalez <albanisy@gmail.com>
2023-05-22 13:42:01 +0200

    tabla usuario perzonaliada

---
+++ modulo3-angular-desarrollo/6.AngularMaterial/proyecto13/src/app/user-table/user-table.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { IUser } from '../user-dir/user.model';
+
+@Component({
+  selector: 'app-user-table',
+  templateUrl: './user-table.component.html',
+  styleUrls: ['./user-table.component.scss']
+})
+export class UserTableComponent {
+displayedColumns:string[] = ['avatar','fullName', 'email','actions' ];
+users:IUser[] =[
+  {
+    fullName: "Juan Garcia",
+    email: "usuario1@gmail.com",
+    bio: "Lorem ipsum dolor asit amet ipsum",
+    avatar: "https://ui-avatars.com/api/?name=Usuario1+Garcia"
+  },
+  {
+    fullName: "Patricia Martinez",
+    email: "usuario2@gmail.com",
+    bio: "Lorem ipsum dolor asit amet ipsum",
+    avatar: "https://ui-avatars.com/api/?name=Usuario1+Garcia"
+  },
+  {
+    fullName: "Antonio Rataplan",
+    email: "usuario3@gmail.com",
+    bio: "Lorem ipsum dolor asit amet ipsum",
+    avatar: "https://ui-avatars.com/api/?name=Usuario1+Garcia"
+  },
+  {
+    fullName: "Natalia rivas",
+    email: "usuario4@gmail.com",
+    bio: "Lorem ipsum dolor asit amet ipsum",
+    avatar: "https://ui-avatars.com/api/?name=Usuario1+Garcia"
+  },
+  {
+    fullName: "Gabriela Sotano",
+    email: "usuario5@gmail.com",
+    bio: "Lorem ipsum dolor asit amet ipsum",
+    avatar: "https://ui-avatars.com/api/?name=Usuario1+Garcia"
+  },
+]
+}
+
