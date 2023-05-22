fab48a148a40406607f48fa01c74c4fbb2766944
AlbanisGonzalez <albanisy@gmail.com>
2023-05-22 10:34:19 +0200

    angular matterial componente periodic para tablas

--- modulo3-angular-desarrollo/5.Enrutado/proyecto12/src/app/app.module.ts
+++ modulo3-angular-desarrollo/5.Enrutado/proyecto12/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
-import { PhotoGalleryComponent } from './photos/photo-gallery/photo-gallery.component';
+import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotoGalleryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PhotosModule
+    PhotosModule,
+    UsersModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
