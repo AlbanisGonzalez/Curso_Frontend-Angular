c0420801cd4ea6d9fa3e6d742201e8cdc2e751bc
AlbanisGonzalez <albanisy@gmail.com>
2023-05-25 14:09:37 +0200

    Angular formularios input archivos imagen

--- modulo3-angular-desarrollo/7. Formularios/proyecto15/src/app/user-form/user-form.component.ts
+++ modulo3-angular-desarrollo/7. Formularios/proyecto15/src/app/user-form/user-form.component.ts
@@ -4,39 +4,52 @@ import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.scss']
+  styleUrls: ['./user-form.component.scss'],
 })
 export class UserFormComponent {
-userForm = new FormGroup({
-  email: new FormControl(''),
-  fullName: new FormControl(''),
-  age:new FormControl(''),
-  salary:new FormControl('12600'),
-  isStudent: new FormControl(true),
-  birthday: new FormControl(null),
-  entryHour: new FormControl(null),
-  bookingDate: new FormControl(null),
-  jobCategory: new FormControl(''),
-  company: new FormControl(''), // Nota: no poner null ni dejarlo sin string
-  hobbies: new FormControl([]),// selector multilple
+  userForm = new FormGroup({
+    email: new FormControl(''),
+    fullName: new FormControl(''),
+    age: new FormControl(''),
+    salary: new FormControl('12600'),
+    isStudent: new FormControl(true),
+    birthday: new FormControl(null),
+    entryHour: new FormControl(null),
+    bookingDate: new FormControl(null),
+    jobCategory: new FormControl(''),
+    company: new FormControl(''), // Nota: no poner null ni dejarlo sin string
+    hobbies: new FormControl([]), // selector multilple
+    bio: new FormControl(''),
+    avatar: new FormControl(), // archivo
+  });
 
-});
-save(): void {
-  console.log(this.userForm.get('email')?.value);
-  console.log(this.userForm.get('fullName')?.value);
-  console.log(typeof this.userForm.get('age')?.value);
-  console.log(this.userForm.get('salary')?.value);
-  console.log(this.userForm.get('isStudent')?.value);
-  console.log(typeof this.userForm.get('isStudent')?.value)
-  console.log(this.userForm.get('birthday')?.value);
-  console.log(this.userForm.get('entryHour')?.value);
-  console.log(typeof this.userForm.get('bookingDate')?.value);
-  console.log(this.userForm.get('jobCategory')?.value);
-  console.log(this.userForm.get('company')?.value);
+  save(): void {
+    console.log(this.userForm.get('email')?.value);
+    console.log(this.userForm.get('fullName')?.value);
+    console.log(typeof this.userForm.get('age')?.value);
+    console.log(this.userForm.get('salary')?.value);
+    console.log(this.userForm.get('isStudent')?.value);
+    console.log(typeof this.userForm.get('isStudent')?.value);
+    console.log(this.userForm.get('birthday')?.value);
+    console.log(this.userForm.get('entryHour')?.value);
+    console.log(typeof this.userForm.get('bookingDate')?.value);
+    console.log(this.userForm.get('jobCategory')?.value);
+    console.log(this.userForm.get('company')?.value);
+    console.log(this.userForm.get('bio')?.value);
+    console.log(this.userForm.get('avatar')?.value);
+    this.userForm.reset();
+  }
+  imageSrc: string | undefined;
 
+  uploadFile(event: Event): void {
+    let target = event.target as HTMLInputElement;
 
-
-
-  this.userForm.reset();
-}
+    if (target.files !== null && target.files.length > 0) {
+      let fileImg = target.files[0];
+      //Opcional: mostrat la imagen al usuario
+      let reader = new FileReader();
+      reader.onload = ev=> this.imageSrc = reader.result as string; // Qué hacer cuando se lea la imagen
+      reader.readAsDataURL(fileImg); // Leer la imagen
+    }
+  }
 }
