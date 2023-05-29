57f536107332a71603f6a9d8033ba876b6a298be
AlbanisGonzalez <albanisy@gmail.com>
2023-05-29 12:14:16 +0200

    Angular formularios pattern teléfono

--- modulo3-angular-desarrollo/7. Formularios/proyecto15/src/app/user-form-validation/user-form-validation.component.ts
+++ modulo3-angular-desarrollo/7. Formularios/proyecto15/src/app/user-form-validation/user-form-validation.component.ts
@@ -10,7 +10,10 @@ export class UserFormValidationComponent  {
 
   userForm = new FormGroup({
     fullName: new FormControl('',[Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email])
+    nickName: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]),
+    salary: new FormControl(null, [Validators.required, Validators.min(16500), Validators.max(100000)]),
+    nif: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{8}[A-Z]$')]),
+    phone: new FormControl(null, [Validators.pattern('^[679]{1}[0-9]{8}$')])
   });
 
   save(): void {
@@ -19,7 +22,7 @@ export class UserFormValidationComponent  {
     } else {
       console.log("Formulario incorrecto, tiene errores de validación");
     }
-    console.log(this.userForm.get('fullName'));
+    console.log(this.userForm.get('nickName'));
   }
 
 
