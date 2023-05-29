19eb30e0ae5433f67d0b0f6880a76cfa4e6346c3
AlbanisGonzalez <albanisy@gmail.com>
2023-05-29 10:46:05 +0200

    Validación formularios Validators. required

--- modulo3-angular-desarrollo/7. Formularios/proyecto15/src/app/user-form-validation/user-form-validation.component.ts
+++ modulo3-angular-desarrollo/7. Formularios/proyecto15/src/app/user-form-validation/user-form-validation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form-validation',
@@ -9,49 +9,19 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class UserFormValidationComponent  {
 
   userForm = new FormGroup({
-    email: new FormControl(''),
-    fullName: new FormControl(''),
-    age: new FormControl(''),
-    salary: new FormControl('12600'),
-    isStudent: new FormControl(true),
-    birthday: new FormControl(null),
-    entryHour: new FormControl(null),
-    bookingDate: new FormControl(null),
-    jobCategory: new FormControl(''),
-    company: new FormControl(''), // Nota: no poner null ni dejarlo sin string
-    hobbies: new FormControl([]), // selector multilple
-    bio: new FormControl(''),
-    avatar: new FormControl(), // archivo
+    fullName: new FormControl('',[Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email])
   });
 
   save(): void {
-    console.log(this.userForm.get('email')?.value);
-    console.log(this.userForm.get('fullName')?.value);
-    console.log(typeof this.userForm.get('age')?.value);
-    console.log(this.userForm.get('salary')?.value);
-    console.log(this.userForm.get('isStudent')?.value);
-    console.log(typeof this.userForm.get('isStudent')?.value);
-    console.log(this.userForm.get('birthday')?.value);
-    console.log(this.userForm.get('entryHour')?.value);
-    console.log(typeof this.userForm.get('bookingDate')?.value);
-    console.log(this.userForm.get('jobCategory')?.value);
-    console.log(this.userForm.get('company')?.value);
-    console.log(this.userForm.get('bio')?.value);
-    console.log(this.userForm.get('avatar')?.value);
-    this.userForm.reset();
-  }
-  imageSrc: string | undefined;
-
-  uploadFile(event: Event): void {
-    let target = event.target as HTMLInputElement;
-
-    if (target.files !== null && target.files.length > 0) {
-      let fileImg = target.files[0];
-      //Opcional: mostrat la imagen al usuario
-      let reader = new FileReader();
-      reader.onload = ev=> this.imageSrc = reader.result as string; // Qué hacer cuando se lea la imagen
-      reader.readAsDataURL(fileImg); // Leer la imagen
+    if(this.userForm.valid) {
+      console.log("Formulario correcto");
+    } else {
+      console.log("Formulario incorrecto, tiene errores de validación");
     }
+    console.log(this.userForm.get('fullName'));
   }
+
+
 }
 
