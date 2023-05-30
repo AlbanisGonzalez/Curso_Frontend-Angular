06f7fc3b4aef2f5063c9eeb5a8b9c07e61cb94e6
AlbanisGonzalez <albanisy@gmail.com>
2023-05-30 12:36:41 +0200

    Angular material formularios campo password type dinámico

--- modulo3-angular-desarrollo/7. Formularios/proyecto16/src/app/user-form/user-form.component.ts
+++ modulo3-angular-desarrollo/7. Formularios/proyecto16/src/app/user-form/user-form.component.ts
@@ -7,12 +7,14 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent {
+  hide = true;
   userForm = new FormGroup({
     fullName: new FormControl('',[Validators.required]),
     bio: new FormControl('', [Validators.required, Validators.maxLength(256)]),
     email: new FormControl('', [Validators.required, Validators.email]),
     isStudent: new FormControl(false),
     acceptConditions: new FormControl(false, [Validators.requiredTrue]),
+    password: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z0-9$%&/()]{8,20}$')]),
 
 
       // Password con opción de ver contraseña
