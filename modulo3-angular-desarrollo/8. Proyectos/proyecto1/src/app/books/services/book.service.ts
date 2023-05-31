060e71153bee8ab6f1c915b8e560886434b8bd38
AlbanisGonzalez <albanisy@gmail.com>
2023-05-31 14:14:15 +0200

    Angular proyecto1 modelo y servicio

--- modulo3-angular-desarrollo/8. Proyectos/proyecto1/src/app/books/services/book.service.ts
+++ modulo3-angular-desarrollo/8. Proyectos/proyecto1/src/app/books/services/book.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { IBook } from '../models/book.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+  url: string = "http://localhost:3000/books";
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) { }
+
+  findAll(): Observable<IBook[]> {
+    return this.httpClient.get<IBook[]>(this.url);
+  }
+
+  findById(id: number): Observable<IBook> {
+    // https://jsonplaceholder.typicode.com/todos/4
+    return this.httpClient.get<IBook>(`${this.url}/${id}`);
+  }
+
+  // create: Crear un nuevo objeto TODO Event en el servidor
+  create(book:IBook): Observable<IBook> {
+    return this.httpClient.post<IBook>(this.url, book);
+  }
+
+
+  // update
+  update(book: IBook): Observable<IBook> {
+    return this.httpClient.put<IBook>(`${this.url}/${event.id}`, event);
+  }
+
+  // deleteById
+  deleteById(id: number): void {
+    this.httpClient.delete(`${this.url}/${id}`);
+  }
 }
