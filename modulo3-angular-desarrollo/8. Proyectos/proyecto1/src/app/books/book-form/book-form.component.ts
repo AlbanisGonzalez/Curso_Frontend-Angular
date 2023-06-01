3b7c4b64fe8bb273137eae1c3cd344d5044294dc
AlbanisGonzalez <albanisy@gmail.com>
2023-06-01 14:17:03 +0200

    Angular proyecto1 BookFormComponent

--- modulo3-angular-desarrollo/8. Proyectos/proyecto1/src/app/books/book-form/book-form.component.ts
+++ modulo3-angular-desarrollo/8. Proyectos/proyecto1/src/app/books/book-form/book-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-form',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class BookFormComponent {
 
+  bookForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]),
+    sinopsis: new FormControl('', [Validators.maxLength(1000)]),
+    releases: new FormControl(null, [Validators.required]),
+    numPages: new FormControl(0, [Validators.min(30)]),
+    price: new FormControl(0, [Validators.required, Validators.min(5), Validators.max(500)]),
+    // photo: new FormControl(''),
+    // authorId: new FormControl(null,[Validators.required])
+  });
 }
--- modulo3-angular-desarrollo/8. Proyectos/proyecto1/src/app/books/books-routing.module.ts
+++ modulo3-angular-desarrollo/8. Proyectos/proyecto1/src/app/books/books-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
       component: BookListComponent
     },
     {
-      path: 'id',
+      path: ':id',
       component: BookDetailComponent
     },
     {
