f888816b05f0244e75567b3e7bd209226dbb9041
AlbanisGonzalez <albanisy@gmail.com>
2023-06-07 11:05:16 +0200

    Angular proyectos filtro en listado libros booklist

--- modulo3-angular-desarrollo/8. Proyectos/proyecto1/src/app/books/book-list/book-list.component.ts
+++ modulo3-angular-desarrollo/8. Proyectos/proyecto1/src/app/books/book-list/book-list.component.ts
@@ -1,6 +1,8 @@
+import { AuthorService } from 'src/app/authors/services/author.service';
 import { BookService } from './../services/book.service';
 import { Component, OnInit } from '@angular/core';
 import { IBook } from '../models/book.model';
+import { IAuthor } from 'src/app/authors/models/author.model';
 
 @Component({
   selector: 'app-book-list',
@@ -10,14 +12,20 @@ import { IBook } from '../models/book.model';
 export class BookListComponent implements OnInit {
   displayedColumns: string[] = ['title', 'sinopsis', 'release', 'numPages', 'price', 'actions'];
   books: IBook[] = [];
+  authors: IAuthor[] = [];
 
-  constructor(private bookService: BookService) { }
+
+  constructor(private bookService: BookService,
+    private authorService: AuthorService
+  ) { }
 
   ngOnInit(): void {
     this.bookService.findAll().subscribe(data => this.books = data);
+    this.authorService.findAll().subscribe(data => this.authors = data);
+
   }
 
-  deleteBook(book: IBook){
+  deleteBook(book: IBook) {
 
   }
 }
