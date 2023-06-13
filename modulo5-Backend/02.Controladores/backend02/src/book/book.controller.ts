3d3d703867e11d8a9225abe98aa8c34fe40f200a
AlbanisGonzalez <albanisy@gmail.com>
2023-06-13 12:31:05 +0200

    Nest book.controller y book.model

--- modulo5-Backend/02.Controladores/backend02/src/book/book.controller.ts
+++ modulo5-Backend/02.Controladores/backend02/src/book/book.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { IBook } from './book.model';
 
-@Controller('book')
-export class BookController {}
+@Controller('books')
+export class BookController {
+    //http:localhost:3000/book/
+    @Get()
+    findAll(): IBook[] {
+        let book1: IBook = {
+            id: 1,
+            title: 'Libro1',
+            price: 29.99,
+        };
+        let book2: IBook = {
+            id: 2,
+            title: 'Libro2',
+            price: 29.99,
+        };
+        return [book1, book2]
+    }
+}
