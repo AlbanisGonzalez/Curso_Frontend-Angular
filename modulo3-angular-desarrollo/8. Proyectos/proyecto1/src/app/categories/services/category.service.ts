076abe1ea278143958cfc212692711c85ded5d08
AlbanisGonzalez <albanisy@gmail.com>
2023-06-13 10:33:06 +0200

    Corrección modulo category

--- modulo3-angular-desarrollo/8. Proyectos/proyecto1/src/app/categories/services/category.service.ts
+++ modulo3-angular-desarrollo/8. Proyectos/proyecto1/src/app/categories/services/category.service.ts
@@ -1,15 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { ICategory } from '../models/category.model';
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-  findById(id: number) {
-    throw new Error('Method not implemented.');
-  }
-  findAll() {
-    throw new Error('Method not implemented.');
+
+  url: string = "http://localhost:3000/categories";
+
+  constructor(private httpClient: HttpClient) { }
+
+  findAll(): Observable<ICategory[]> {
+    return this.httpClient.get<ICategory[]>(this.url);
   }
 
-  constructor() { }
+  findById(id: number): Observable<ICategory> {
+    return this.httpClient.get<ICategory>(`${this.url}/${id}`);
+  }
+
+  create(category :ICategory): Observable<ICategory> {
+    return this.httpClient.post<ICategory>(this.url, category);
+  }
+
+  update(category: ICategory): Observable<ICategory> {
+    return this.httpClient.put<ICategory>(`${this.url}/${category.id}`, category);
+  }
+
+  deleteById(id: number): void {
+    this.httpClient.delete(`${this.url}/${id}`);
+  }
 }
