6c68edff027e9372404d3a38f67f1d905b970ebc
AlbanisGonzalez <albanisy@gmail.com>
2023-06-13 11:00:33 +0200

    Nest controlador con métodos

---
+++ modulo5-Backend/02.Controladores/backend02/dist/app.controller.d.ts
@@ -0,0 +1,11 @@
+import { AppService } from './app.service';
+export declare class AppController {
+    private readonly appService;
+    constructor(appService: AppService);
+    getHello(): string;
+    getHello2(): string;
+    getHello3(): string;
+    getHello4(): number;
+    getHello5(): any;
+    getHello6(): any;
+}
--- modulo5-Backend/02.Controladores/backend02/src/app.controller.ts
+++ modulo5-Backend/02.Controladores/backend02/src/app.controller.ts
@@ -9,4 +9,45 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('hello2')
+  getHello2(): string {
+    return 'Hola2 desde nestjs';
+  }
+  @Get('hello3')
+  getHello3(): string {
+    return 'Hola3 desde nestjs';
+  }
+  @Get('hello4')
+  getHello4(): number {
+    return 100;
+  }
+  @Get('hello5')
+  getHello5(): any {
+    return {
+      id: 1,
+      fullName: 'HelloMan',
+      age: 25,
+    };
+  }
+  @Get('hello6')
+  getHello6(): any {
+    return [
+      {
+        id: 1,
+        fullName: 'HelloWoman',
+        age: 25,
+      },
+      {
+        id: 2,
+        fullName: 'HelloMan',
+        age: 25,
+      },
+      {
+        id: 3,
+        fullName: 'HelloMachine',
+        age: 25,
+      },
+    ];
+  }
 }
