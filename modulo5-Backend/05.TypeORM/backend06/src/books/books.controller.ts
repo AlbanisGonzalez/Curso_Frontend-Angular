02cf6b70e3a7b780d7e2c488216fc8092139cc51
AlbanisGonzalez <albanisy@gmail.com>
2023-06-21 13:24:22 +0200

    Nest backend06 proyecto base asociaciones entre entidades

---
+++ modulo5-Backend/05.TypeORM/backend06/src/books/books.controller.ts
@@ -0,0 +1,73 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { Book } from './books.model';
+
+@Controller('books')
+export class BooksController {
+
+    constructor(private bookService: BooksService) { }
+
+    @Get()
+    findAll(): Promise<Book[]> {
+        return this.bookService.findAll();
+    }
+
+    @Get(':id')
+    findById(@Param("id") id: number): Promise<Book | null> {
+        return this.bookService.findById(id);
+    }
+
+    @Get('title-eq/:title')
+    findAllByTitleEquals(@Param("title") title: string): Promise<Book[]> {
+        return this.bookService.findAllByTitleEquals(title);
+    }
+
+    @Get('title-like/:title')
+    findAllByTitleLike(@Param('title') title: string): Promise<Book[]> {
+        return this.bookService.findAllByTitleLike(title);
+
+    }
+    @Get('price/min/:min/max/:max')
+    findAllByPriceBetween(
+        @Param('min') min: number,
+        @Param('max') max: number): Promise<Book[]> {
+        return this.bookService.findAllByPriceBetween(min, max);
+    }
+    @Get('published')
+    findAllByPublishedTrue(): Promise<Book[]> {
+        return this.bookService.findAllByPublishedTrue();
+
+    }
+    @Get('quantity/:quantity/price/price')
+    findAllByQuantityAndPrice(
+        @Param('quantity') quantity: number,
+        @Param('price') price: number): Promise<Book[]> {
+        return this.bookService.findAllByPublishedTrue();
+    }
+
+    @Get('ordered-by-price-asc')
+    findAllOrderByPriceAsc(): Promise<Book[]> {
+        return this.bookService.findAllOrderByPriceAsc();
+
+    }
+
+    @Post()
+    async create(@Body() book: Book): Promise<Book> {
+        return await this.bookService.create(book);
+    }
+
+    @Put()
+    async update(@Body() book: Book): Promise<Book> {
+        return await this.bookService.update(book);
+    }
+
+    @Delete(':id')
+    async deleteById(
+        @Param('id', ParseIntPipe) id: number
+    ): Promise<void> {
+        return await this.bookService.deleteById(id);
+    }
+
+
+
+}
