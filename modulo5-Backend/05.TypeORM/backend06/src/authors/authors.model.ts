c1dd03ef750037f5109d772a400c9444f8e71bc7
AlbanisGonzalez <albanisy@gmail.com>
2023-06-21 14:02:31 +0200

    Nest authors module

---
+++ modulo5-Backend/05.TypeORM/backend06/src/authors/authors.model.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@Entity()
+export class Author {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 50 })
+    name: string
+
+    @Column({ length: 50 })
+    email: string;
+
+    @Column({ type: 'text' })
+    bio: string;
+
+    @CreateDateColumn({ name: 'created_date' })
+    createdDate: Date;
+
+    @UpdateDateColumn({name:'updated_date'})
+    updatedDate: Date;
+
+
+}
