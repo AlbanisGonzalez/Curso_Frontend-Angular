f26a57b5d9aa80833f92fb228eeb0aaf35c486d6
AlbanisGonzalez <albanisy@gmail.com>
2023-06-26 14:09:58 +0200

    Nest authorService

--- modulo5-Backend/05.TypeORM/backend06/src/locations/locations.module.ts
+++ modulo5-Backend/05.TypeORM/backend06/src/locations/locations.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LocationsController } from './locations.controller';
 import { LocationsService } from './locations.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Location])],
   controllers: [LocationsController],
   providers: [LocationsService]
 })
